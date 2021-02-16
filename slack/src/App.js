36195a7329a2acb545c948bd85a52cc0d378bf21
Rama Krishna Parjanya Varanasi <krishh17.varanasi@gmail.com>
2021-02-16 19:26:45 +0530

    Sidebar-1

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
     <div className="App">
       <h1> Slack-clone </h1>
-
       <Header />
-      
+
+      <div className='app_body'>
+        <Sidebar />
+        
+      </div>
+
+
     </div>
   );
 }
